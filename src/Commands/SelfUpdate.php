a140189787c585eddcc8bc5480269c13b3e17225
MGatner <mgatner@icloud.com>
2020-05-21 00:38:22 +0000

    Begin command

--- src/Commands/SelfUpdate.php
+++ src/Commands/SelfUpdate.php
@@ -14,10 +14,70 @@ class SelfUpdate extends BaseCommand
     protected $description = 'Check for Composer updates then patch this project.';
     protected $usage       = 'selfupdate';
 
+	/**
+	 * The library instance
+	 *
+	 * @var Tatter\Patches\Patches
+	 */
+	public $patches;
+
+	/**
+	 * Run through the entire patch process with feedback and interaction.
+	 */
     public function run(array $params)
     {
-		$patches = new Patches();
+		CLI::write('Beginning patch process');
+		$this->patches = new Patches();
 
-		$patches->run();
+		// Display config info
+		$codex   = $this->patches->getCodex();
+		$sources = $this->patches->getSources();
+
+		CLI::write('Using the following configuration:');
+		CLI::table([
+			['Updater',   $codex->config->updater],
+			['Merger',    $codex->config->merger],
+			['Base Path', $codex->config->basePath],
+			['Project',   $codex->config->rootPath],
+			['Deletes?',  $codex->config->allowDeletes ? 'Allowed' : 'Disabled'],
+			['Events?',   $codex->config->allowEvents ? 'Allowed' : 'Disabled'],
+			['Sources',   $sources ? implode(', ', array_keys($sources)) : 'None detected'],
+			['Ignored',   $codex->config->ignoredSources ? implode(', ', $codex->config->ignoredSources) : 'None'],
+		]);
+
+		$this->menu();
+return;
+		// Run everything up to the merge
+		$this->patches->beforeUpdate();
+		$this->patches->update();
+		$this->patches->afterUpdate();
+
+		$this->menu();
+
+		// Call the chosen merging method
+		$this->patches->merge();
+
+		// Write out the Codex
+		$this->patches->getCodex->save();
+
+		return $result;
 	}
+
+	/**
+	 * Display and process the main menu
+	 */
+    public function menu()
+    {
+		CLI::write('What would you like to do:');
+		CLI::write('(M)erge the files');
+		CLI::write('(S)how all files');
+		CLI::write('Show (C)hanged files');
+		CLI::write('Show (A)dded files');
+
+    	switch (CLI::prompt('Selection?', ['m', 's', 'c', 'a']))
+    	{
+    		default:
+    			CLI::write('Good choice!');
+    	}
+    }
 }
